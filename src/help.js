0dfde7243d55baae13373c7671947c8eabf1d7a2
VitorOlive <76225774+VitorOlive@users.noreply.github.com>
2021-01-12 11:52:18 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,229 @@
+const help = (prefix) => {
+	return `💎 Comandos do VitinnBot 💎  
+🔰 Principais Comandos 🔰 
+
+Comando: '${prefix}sticker' ou '${prefix}stiker'
+Função: converter imagem / gif / vídeo em adesivo
+Como Usar: responder imagem / gif / vídeo ou enviar imagem / gif / vídeo com legenda 
+
+Comando: '${prefix}sticker nobg' ou '${prefix}stiker nobg'
+Função: converter imagem em adesivo removendo o fundo
+Como Usar: responder a imagem, ou enviar com legenda
+
+Comando: '${prefix}toimg'
+Função: converter adesivos em imagem
+Como Usar: responder a sticker 
+
+Comando:'${prefix}criador'
+Função: Mostra as informações do criador do bot
+
+🔰 Comandos de Memes 🔰  
+
+Comando: '${prefix}meme'
+Função: imagens aleatórias de meme [english]
+
+Comando: '${prefix}memeindo'
+Função: imagens aleatórias de meme [indo]
+
+🔰 Comandos +18 🔰
+
+Comando: '${prefix}loli'
+Função: imagens aleatórias de loli
+
+Comando: '${prefix}nekoanime'
+Função: imagens aleatórias de neko
+
+Comando '${prefix}randomanime'
+Função: imagem aleatoria de anime
+
+Comando '${prefix}randomhentai'
+Função: imagem aleatoria de hentai
+
+Comando: '${prefix}nsfwblowjob'
+Função: gifs de blowjobs de hentai
+
+Comando: '${prefix}nsfwneko'
+Função: imagem de hentai neko
+
+Comando: '${prefix}nsfwtrap'
+Função: imagem de hentai trap
+
+Comando: '${prefix}nsfw'
+Função: Ativa o modo hentai no grupo
+Como Usar: '${prefix}nsfw 1' para ativar o modo nsfw e '${prefix}nsfw 0' para desativar o modo nsfw
+Obs: Este comando pode ser usado se você for um administrador do grupo 
+
+Comando: '${prefix}nsfwloli'
+Função: imagens aleatórias de hentai
+Como Usar: basta enviar o comando
+Obs: Funciona apenas no pv do bot ou se for administrador do grupo
+
+Comando: '${prefix}shota'
+Função: imagens aleatórias de shotas(lolis masculinas)
+Como Usar: apenas envie o comando
+
+Comando: '${prefix}pack'
+Função: Enviar um pacote de links +18
+Como Usar: Basta enviar o comando
+
+
+🔰 Comandos de texto 🔰 
+
+Comando: '${prefix}frase'
+Função: Envia uma frase filosofica
+
+Comando: '${prefix}textph'
+Função: converte o texto na logo da pornhub
+Como Usar: '${prefix}textph [pt1] | [pt2]'
+
+Comando: '${prefix}traduz'
+Função: traduz seu texto em outra lingua
+Como Usar: '${prefix}traduz [la] [texto]'
+Exemplo: '${prefix}traduz pt hello my friends'
+
+Comando: '${prefix}idioma'
+Função: lista todos os códigos de idioma
+
+Comando: '${prefix}text3d'
+Função: converte o texto em imagem 3d
+Como Usar: '${prefix}text3d [pt1]'
+
+Comando: '${prefix}wolflogo'
+Função: converte o texto em imagem com o logo de um lobo
+Como Usar: '${prefix}wolflogo [pt1] | [pt2]'
+
+Comando: '${prefix}lionlogo'
+Função: converte o texto em imagem com o logo de um leao
+Como Usar: '${prefix}lionlogo [pt1] | [pt2]'
+
+Comando: '${prefix}glitch'
+Função: converte o texto em imagem com estilo glitch
+Como Usar: '${prefix}glitch [pt1] | [pt2]'
+
+Comando:'${prefix}ninjalogo'
+Função: converte o texto em imagem com o logo de um ninja
+Como Usar:'${prefix}ninjalogo [pt1] | [pt2]'
+
+Comando: '${prefix}neontec'
+Função: converte o texto em imagem com estilo neon tecnology
+Como Usar: '${prefix}neontec [pt1]'
+
+Comando:'${prefix}neonligth'
+Função: converte o texto em imagem com estilo neon ligth
+Como Usar:'${prefix}neonligth [pt1]'
+
+Comando:'${prefix}snow'
+Função: converte o texto em imagem com estilo em snow'
+Como Usar:'${prefix}snow [pt1]'
+
+Comando:'${prefix}blood'
+Função: converte o texto em imagem com estilo blood
+Como Usar:'${prefix}blood [pt1]'
+
+Comando:'${prefix}dropwater'
+Função: converte o texto em imagem com estilo dropwater
+Como Usar:'${prefix}dropwater [pt1]'
+
+🔰 Outros Comandos 🔰 
+
+Comando: '${prefix}gtts'
+Função: converter texto em fala / áudio
+Como Usar: '${prefix}gtts [la] [texto]'
+Exemplo: '${prefix}gtts ja Oni-chan'
+
+Comando: '${prefix}signome'
+Função: Mostra o significado do seu nome
+Como usar:'${prefix}signome [nome]'
+Exemplo:'${prefix}signome ian'
+
+Comando: '${prefix}nethunter'
+Função: Mostra um tutorial de como ter um emulador de kali linux no seu termux
+
+Comando: '${prefix}db'
+Função: envia links de várias database de travazap
+
+Comando:'${prefix}wppim'
+Função: envia links de todos whatsapp imunes
+
+Comando: '${prefix}url2img'
+Função: tirar uma captura de tela na web
+Como Usar: *${prefix}url2img [tip] [url]*
+
+Comando: '${prefix}simi'
+Função: sua mensagem será respondida por simi
+Como Usar: '${prefix}simi [texto]'
+
+Comando: '${prefix}ocr'
+Função: pegue o texto na foto
+Como Usar: responder imagem ou enviar imagem com legenda 
+
+Comando: '${prefix}wait'
+Função: Pesquisa o anime na imagem [ O que anime é isso / aquilo ]
+Como Usar: responder imagem ou enviar imagem com legenda 
+
+Comando: '${prefix}setprefix'
+Função: substituir prefixo
+Como Usar: '${prefix}setprefix [text|optional]' 
+Exemplo: '${prefix}setprefix ?'
+
+🔰 Comandos de Grupos 🔰
+
+comando : *${prefix}add*
+desc : adicionar membro ao grupo
+uso : *${prefix}add 55 + numero com DDD*
+Nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é o admin! \  
+
+comando : *${prefix}kick*
+desc : expulsar membros do grupo
+uso : *${prefix}kick @tagmember*
+Nota :  só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!
+
+comando : *${prefix}promote*
+uso : *${prefix}promote @tagmember*
+Nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!
+
+comando : *${prefix}demote*
+desc : tornar o administrador do grupo como membro do grupo
+uso: * $ {prefix}demote @tagmember * 
+nota: só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!
+
+Comandos: '${prefix}termux'
+Função: Mostra uma lista de comandos básicos do termux
+
+Comandos: '${prefix}welcome'
+Função: Ativa o modo boas-vindas do grupo
+Como Usar: '${prefix}welcome 1' para ativar o modo welcome e '${prefix}welcome 0' para desativar o modo welcome
+Obs: Este comando pode ser usado se você for um administrador do grupo 
+
+Comandos: '${prefix}linkgroup'
+Função: pegue o link do grupo
+Como Usar: just send the command
+Obs: só pode ser usado quando o bot se torna admin, e quem enviar o comando é admin! 
+
+Comandos: '${prefix}tagall'
+Função: marca todos os membros do grupo, incluindo administradores também
+Como Usar: apenas envie o comando
+Obs: Este comando pode ser usado se você for um administrador do grupo 
+
+Comandos: '${prefix}simih'
+Função: ative o modo simi no grupo
+Como Usar: '${prefix}simih 1' para ativar o modo simih e '${prefix}simih 0' para desativar o modo simih
+Obs: Este comando pode ser usado se você for um administrador do grupo 
+
+╔════════════════════
+  FEITO POR *Vitinn*
+  DUVIDAS? 👇
+  WA.me/5598989016015
+  
+  Vitinn domina 🤙🥶
+╚════════════════════
+
+`
+}
+
+exports.help = help
+
+
+
+
+
