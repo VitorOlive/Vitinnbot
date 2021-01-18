cb2391361fa1c0f3cfcdfaa97fc13412fbcef6e5
VitorOlive <76225774+VitorOlive@users.noreply.github.com>
2021-01-18 15:30:17 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,124 @@
+const help = (prefix) => {
+	return `╠════════════════════
+║   ⏤͟͟͞͞𝐕𝐈𝐓𝐈𝐍𝐍⛧𝐁𝚯𝐓
+	
+➸ Prefix:  *「. 」*
+➸ Status: *「 Online 」*
+
+
+╠════✪〘 FIGURINHA 〙✪═════
+
+╠➣comando : *${prefix}sticker* or *${prefix}     stiker*
+╠➣desc : converter imagem / gif / vídeo para sticker
+╠➣uso : responder imagem / gif / vídeo ou enviar imagem / gif / vídeo com legenda \ n
+╠════════════════════
+╠➣comando : *${prefix}sticker nobg* or *${prefix}stiker nobg*
+╠➣desc : converter imagem em adesivo removendo o fundo
+╠➣uso : responder imagem ou enviar imagem com legenda \ n
+╠════════════════════
+╠➣comando : *${prefix}toimg*
+╠➣desc : converter sticker para a imagem
+╠➣uso : resposta sticker\n
+╠════════════════════
+╠➣comando : *${prefix}tsticker* or *${prefix}tstiker*
+╠➣desc: converter texto para sticker
+╠➣uso : *${prefix}tsticker text in here*\n
+
+╠═════✪〘 MEME 〙✪══════
+
+╠➣comando : *${prefix}meme*
+╠➣desc : imagens aleatórias de meme [english]
+╠➣uso : basta enviar o comando \n
+
+╠════════════════════
+
+╠➣comando : *${prefix}memeindo*
+╠➣desc : imagens aleatórias de meme [indo]
+╠➣uso : basta enviar o comando \ n
+
+╠═════✪〘 OUTROS 〙✪═════  
+
+╠➣comando : *${prefix}gtts*
+╠➣desc : converte mensagem em audio/audio
+╠➣uso : *${prefix}gtts [cc] [text]*\nexample : *${prefix}gtts ja On2-chan*\n
+╠════════════════════
+╠➣comando : *${prefix}loli* [OFF]
+╠➣desc : aleatória imagens loli 
+╠➣uso : basta enviar o comando \ n
+╠════════════════════
+╠➣comando : *${prefix}nsfwloli*
+╠➣desc : imagens aleatórias de nsfw loli
+╠➣uso : basta enviar o comando \ n
+╠════════════════════
+╠➣comando : *${prefix}url2img*
+╠➣desc : tirar screenshots da web
+╠➣uso : *${prefix}url2img [tipe] [url]*\n
+╠════════════════════
+╠➣comando : *${prefix}simi*
+╠➣desc : sua mensagem será respondida por simi
+╠➣uso : *${prefix}simi yourmessage*\n
+╠════════════════════
+╠➣comando : *${prefix}ocr*
+╠➣desc : pegue o texto na foto
+╠➣uso : responder imagem ou enviar imagem com legenda\n
+╠════════════════════
+╠➣comando : *${prefix}wait*
+╠➣desc : procure anime com imagem [ What Anime Is This/That ]
+╠➣uso : responder imagem ou enviar imagem com legenda\n
+╠════════════════════
+╠➣comando : *${prefix}setprefix*
+╠➣desc : substituir prefixo
+╠➣uso : *${prefix}setprefix [text|optional]*
+\nexample : *${prefix}setprefix ?*
+╠➣Nota : Este comando só pode ser usado pelo Vitinn lindo \ n
+
+╠═════✪〘 GRUPO 〙✪═════
+
+╠➣comando : *${prefix}add*
+╠➣desc : adicionar membro ao grupo
+╠➣uso : *${prefix}add 55 + numero com DDD*\n
+╠➣Nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é o admin! \ n
+╠════════════════════
+╠➣comando : *${prefix}kick*
+╠➣desc : expulsar membros do grupo
+╠➣uso : *${prefix}kick @tagmember*\n
+╠➣Nota :  só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!\n
+╠════════════════════
+╠➣comando : *${prefix}promote*
+╠➣uso : *${prefix}promote @tagmember*\n
+╠➣Nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin! \ n
+╠════════════════════
+╠➣comando : *${prefix}demote*
+╠➣desc : tornar o administrador do grupo como membro do grupo
+╠➣uso: * $ {prefix}demote @tagmember * \ n
+╠➣nota: só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!\n
+╠════════════════════
+╠➣comando : *${prefix}linkgroup*
+╠➣desc : pegue o link do grupo
+╠➣uso : apenas envie o comando
+╠➣Nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin! \ n
+╠════════════════════
+╠➣comando : *${prefix}tagall*
+╠➣desc : marca todos os membros do grupo, incluindo administradores também
+╠➣uso : apenas envie o comando
+╠➣Nota : Este comando pode ser usado se você for um administrador de grupo \ n
+╠════════════════════
+╠➣comando : *${prefix}simih*
+╠➣desc : ative o modo simi no grupo
+╠➣uso : *${prefix}simih 1* para ativar o modo simi and *${prefix}simih 0* para desativar o modo simi
+╠➣Nota: este comando pode ser usado se você for um administrador de grupo \ n
+╔════════════════════
+  FEITO PELO *Vitinn*
+  PARA MAIS INFORMAÇÕES,CHAMA 👇
+  Wa.me/5598989016015
+  
+  Vitinn domina 🤙🥶
+╚════════════════════
+
+𝗦𝗨𝗣𝗢𝗥𝗧𝗘 〙〲│〲┴┬➣ Wpp: wa.me/5598989016015 〲︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶︶〲 〲 ║▌│█║▌│ █║▌│█│║▌║〲 ║▌│█║▌│ █║▌│█│║▌║〲〲 Copyright © 𝐕𝐈𝐓𝐈𝐍𝐍 愛 2021
+
+`
+ 
+}
+   
+exports.help = help
